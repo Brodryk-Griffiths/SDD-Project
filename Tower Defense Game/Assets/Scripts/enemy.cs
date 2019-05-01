c5c658d1495b0b9ca9e4cc1505457416db3dbd61
Brodryk Griffiths <brodrykg@gmail.com>
2019-05-02 09:00:03 +1000

    Fixed enemy bouncing at waypoints by increasing detection range slighly

--- Tower Defense Game/Assets/Scripts/enemy.cs
+++ Tower Defense Game/Assets/Scripts/enemy.cs
@@ -17,7 +17,7 @@ public class enemy : MonoBehaviour
     Vector3 dir = target.position - transform.position;
     transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-    if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+    if (Vector3.Distance(transform.position, target.position) <= 0.5f)
     {
       GetNextWaypoint();
     }
