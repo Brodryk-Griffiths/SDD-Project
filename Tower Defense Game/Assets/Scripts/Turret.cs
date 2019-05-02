96d1092e77201e67ecc28e86e432b1daedf151e6
Brodryk Griffiths <brodrykg@gmail.com>
2019-05-02 11:36:22 +1000

    designated a range for turrent and created a wireframe representation temorarily. Added code allowing the turret to track the nearest enemy within its range.

--- Tower Defense Game/Assets/Scripts/Turret.cs
+++ Tower Defense Game/Assets/Scripts/Turret.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Turret : MonoBehaviour
 {
+    public Transform target;
+    public float range = 10f;
+    //public string enemyTag = "Enemy";
+    
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f); //Calls UpdateTarget twice a second
+    }
+
+    //Tracking enemies
+    void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } 
+        else
+        {
+            target = null;
+        }
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (target == null)
+            return;
+
         
     }
+
+    //Draw range wireframe
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
 }
+
+//
--- Tower Defense Game/Assets/Scripts/WaveSpawner.cs
+++ Tower Defense Game/Assets/Scripts/WaveSpawner.cs
@@ -12,7 +12,7 @@ public class WaveSpawner : MonoBehaviour
 
   public Text WaveCounterText;
   private int waveIndex = 0;
-
+  
   public void StartWave ()
   {
     // if (countdown <= 0f)
