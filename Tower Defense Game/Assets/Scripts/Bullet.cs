b7236a63db6f1e3eabec2745c16c78ba350b8ac1
Brodryk Griffiths <brodrykg@gmail.com>
2019-05-04 13:04:17 +1000

    Bullet now moves to hit target. Upon hitting target, bullet and target are destroyed, and a particle effect is spawned. In future enemies will have health and require multiple shots to destroy.

---
+++ Tower Defense Game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Transform target;
+    
+    public float speed = 70f;
+    public GameObject impactEffect;
+    public void Chase (Transform _target)
+    {
+        target = _target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitEnemy();
+            return;
+        }
+
+        transform.Translate (dir.normalized * distanceThisFrame, Space.World);
+
+    }
+
+    void HitEnemy()
+    {
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+
+        Destroy(gameObject);
+        Destroy(target.gameObject);
+        
+    }
+}
