f00bd5b48850f8dbac39242d69d79e2d34c730e1
Brodryk Griffiths <brodrykg@gmail.com>
2019-06-27 09:24:44 +1000

    Added menu screen and have working play button which right now launches the first level

---
+++ Tower Defense Game/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void GoToLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
